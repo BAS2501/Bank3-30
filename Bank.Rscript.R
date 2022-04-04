e35f48893a1f5cc3d136e7343bcfa04bc2e3b566
blade strzelczyk <blade@blades-MacBook-Pro.local>
2022-04-04 08:17:29 -0500

    committing code

---
+++ Bank.Rscript.R
@@ -0,0 +1,13 @@
+bank.df <- UniversalBank
+bank.df <- bank.df[ , -c(1,5)]
+bank.df$Education <- factor(bank.df$Education, levels = c(1, 2, 3),
+                            labels = c("Undergrad", "Graduate", "Advanced/Professional"))
+
+set.seed(2)
+train.index <-sample(c(1:dim(bank.df)[1]), dim(bank.df)[1]*0.6)
+train.df <- bank.df[train.index, ]
+valid.df <- bank.df[-train.index, ]
+
+logit.reg <- glm(bank.df$Personal.Loan ~ ., data = train.df, family = "binomial")
+options(scipen = 999)
+summary(logit.reg)
